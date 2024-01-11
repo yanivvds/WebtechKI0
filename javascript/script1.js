91c91f3c5f21c54d85a389dcf022fa2bb6e9b91b
Jeffkroon <jeffkroon03@icloud.com>
2024-01-11 20:04:09 +0100

    nieuwe unfoldable menu

---
+++ javascript/script1.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menuContainer = document.getElementById("menuContainer");
+    menuContainer.style.display = (menuContainer.style.display === "block") ? "none" : "block";
+}
