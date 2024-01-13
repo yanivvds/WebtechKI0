37df8d01b6a3a96569207811d36cb957a04f9fbf
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-13 18:23:45 +0100

    eerste aantal SQL tables

--- travelsinsider.session.sql
+++ travelsinsider.session.sql
@@ -1,7 +1,31 @@
 -- @block
-CREATE TABLE Users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    bio TEXT,
-    country VARCHAR(2)
+-- Users
+CREATE TABLE User (
+    UserID INT PRIMARY KEY,
+    UserName VARCHAR(255),
+    Email VARCHAR(255),
+    Password VARCHAR(255),
+    RegistrationDate DATE
 );
+
+-- Destinations
+CREATE TABLE Destination (
+    DestinationID INT PRIMARY KEY,
+    DestinationName VARCHAR(255),
+    Continent VARCHAR(255),
+    Description TEXT,
+    ImageURL VARCHAR(255)
+);
+
+-- Reviews
+CREATE TABLE Review (
+    ReviewID INT PRIMARY KEY,
+    UserID INT,
+    DestinationID INT,
+    Rating INT,
+    ReviewText TEXT,
+    ReviewDate DATE,
+    FOREIGN KEY (UserID) REFERENCES User(UserID),
+    FOREIGN KEY (DestinationID) REFERENCES Destination(DestinationID)
+);
+
