134d20687bde298b0ffa6b5bbc7d2eb9e1f0f279
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-14 17:36:40 +0100

    database

--- signupscript.php
+++ signupscript.php
@@ -25,3 +25,8 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 }
 
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+print_r($_POST);
+var_dump($password_hash);
