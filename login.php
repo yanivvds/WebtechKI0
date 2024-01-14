3168dbb46b8965197ac0facbb7f754a3c2507f3b
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-14 21:00:05 +0100

    login veranderd

--- login.php
+++ login.php
@@ -1,15 +1,15 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
+    
     $mysqli = require __DIR__ . "/database.php";
-
+    
     $sql = sprintf("SELECT * FROM Users
-            WHERE email = '%s'",
-            $mysqli->real_escape_string($_POST["email"]));
-
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
     $result = $mysqli->query($sql);
-
+    
     $user = $result->fetch_assoc();
 
     var_dump($user);
