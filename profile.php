e54ca9ec92f47f33640ba258ae3f691b1608e685
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-15 21:54:59 +0100

    styling

--- profile.php
+++ profile.php
@@ -28,35 +28,51 @@ $mysqli->close();
     <link rel="stylesheet" href="stylesheet.css"> 
 </head>
 <body>
-    <?php include 'navbar.php'; ?>
-    <div class="profile">
-        
+<?php include 'navbar.php'; ?>
+    <div class="form-container" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+        <div class="profile">
         <div class="container">
             <h1>User Profile</h1>
             <form action="updateprofile.php" method="POST">
+
+        <div class="input-container ic1">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" value="<?php echo $username; ?>" disabled><br>
+        </div>
 
+        <div class="input-container ic2">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" value="<?php echo $email; ?>" disabled><br>
+        </div>
 
+        <div class="input-container ic1">
             <label for="firstName">First Name:</label>
             <input type="text" id="firstName" name="firstName" value="<?php echo $firstName; ?>"><br>
+        </div>
 
+        <div class="input-container ic2">
             <label for="lastName">Last Name:</label>
             <input type="text" id="lastName" name="lastName" value="<?php echo $lastName; ?>"><br>
-
+        </div>
+        
+        <div class="input-container ic1">
             <label for="city">Home Location:</label>
             <input type="text" id="city" name="city" value="<?php echo $city; ?>"><br>
+        </div>
 
+        <div class="input-container ic2">
             <label for="phoneNumber">Phone Number:</label>
             <input type="tel" id="phoneNumber" name="phoneNumber" value="<?php echo $phoneNumber; ?>"><br>
+        </div>
 
+        <div class="input-container ic1">
             <label for="birthday">Birthday:</label>
             <input type="date" id="birthday" name="birthday" value="<?php echo $birthday; ?>"><br>
+        </div>
 
             <input type="submit" value="Update Profile">
-    </form>
+    </form> 
+            </div>
         </div>
     </div>
 </body>
