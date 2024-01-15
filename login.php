f8b5cb845dbe82cda56be55305121977596bb776
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-15 17:36:13 +0100

    login

--- login.php
+++ login.php
@@ -15,9 +15,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     if ($user) {
         if (password_verify($_POST["password"], $user["password"])) {
-            $_SESSION['email'] = $user['email'];
-            header('Location: index.php');
-            exit();
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index.php");
+            exit;
         } else {
             echo "Incorrect password.";
         }
