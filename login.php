8dfb8db61c6aeeb7990ea3b162a52464f300ed5a
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-15 11:31:47 +0100

    sign up link en sign up goed gemaakt

--- login.php
+++ login.php
@@ -70,6 +70,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             <button type="submit" class="submit">Login</button>
         </form>
+        <p>Don't have an account? <a href="signup.html">Sign up</a></p>
     </div>
 
 
