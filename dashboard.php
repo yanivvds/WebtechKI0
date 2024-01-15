218d90b018b0106e498d34e8cc2e03b3de5651a0
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-15 16:37:22 +0100

    naar php

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inspiration</title>
+    <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
+    <link href="css/stylesheetsubpages.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'navbar.html'; ?>
+</body>
+</html>
+
+
+
+    
