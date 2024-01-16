8a9ec06569222a4ca79d5876d89e1f292418cc65
Jeffkroon <jeffkroon03@icloud.com>
2024-01-16 17:44:31 +0100

    inspiration page bijna af

--- inspiration.php
+++ inspiration.php
@@ -6,43 +6,69 @@
     <title>Inspiration</title>
     <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
     <link href="css/stylesheetsubpages.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-</body>
-</html>
+
 <?php include 'navbar.php'; ?>
-
-
+<div class="containertekst">
+    <h1>Travel Blog</h1>
+    <p>Find inspiration for your next trip in this blog</p>
+</div>
     <div class="container">
         <div class="gallery">
             <div class="gallery__item gallery__item--1">
-                <a href="#"><img src="fotos/rahul-chakraborty-5aN2I852PTs-unsplash.jpg" class="gallery__img" alt="Image 1"></a>
+                <a href="#">
+                    <div class="imagediv">
+                        <h5>Amalfi Coast</h5>
+                        <p class="hide1">Cliffside villages, azure sea, and culinary delights.</p>
+                    </div>
+                    </a>
             </div>
             <div class="gallery__item gallery__item--2">
-                <a href="#"><img src="fotos/charles-postiaux-WOw3Pcdmw4I-unsplash.jpg" class="gallery__img" alt="Image 2"></a>
+                <a href="#">
+                    <div class="imagediv2">
+                        <h5>European underdogs</h5>
+                        <p>Discover budget-friendly charm in Eastern European delights</p>
+                    </div>
+                    </a>
             </div>
             <div class="gallery__item gallery__item--3">
-                <a href="#"><img src="fotos/andreas-rasmussen-GhvpEtG5HMs-unsplash.jpg" class="gallery__img" alt="Image 3"></a>
+                <a href="#">
+                    <div class="imagediv3" alt="Image 3">
+                        <h5>Tokyo Beyond the Bright Lights</h5>
+                        <p>Beneath Tokyo's dazzling skyline lies a world of hidden wonders. Embark on a journey beyond the neon glow, where historic temples whisper tales and culinary delights paint a vibrant portrait. Curiosity beckons, click to unveil Tokyo's best-kept secrets!</p>
+                    </div>
+                </a>
             </div>
             <div class="gallery__item gallery__item--4">
-                <a href="#"><img src="fotos/carles-rabada-yKAgTzz7jWk-unsplash.jpg" class="gallery__img" alt="Image 4"></a>
+                <a href="#">
+                    <div class="imagediv4">
+                        <h5>Surfing Odyssey: Where Waves Meet Wonder</h5>
+                        <p>Explore the World's Best Surf Spots! From Bali's barrels to California's breaks, discover the allure of each coastal gem. Immerse in local vibes, savor seaside delights, and ride the waves of pure serenity. Ready for a surf adventure? Click to chase waves and coastal charm!"</p>
+                    </div>
+                </a>
             </div>
             <div class="gallery__item gallery__item--5">
-                <a href="#"><img src="fotos/fineas-anton-kC1-9Snr9hI-unsplash.jpg" class="gallery__img" alt="Image 5"></a>
+                <a href="#">
+                    <div class="imagediv5">
+                        <h5>Savoring Italy's Culinary Delights</h5>
+                        <p>Savor Italy's Culinary Symphony: Pasta, Pizza, Truffles, Gelato, and Regional Wines. A Gastronomic Journey</p>
+                    </div>
+                </a>
             </div>
             <div class="gallery__item gallery__item--6">
-                <a href="#"><img src="fotos/carles-rabada-yKAgTzz7jWk-unsplash.jpg" class="gallery__img" alt="Image 6"></a>
+                <a href="#">
+                    <div class="imagediv6" alt="Image 6">
+                        <h5>Mystical Mexico</h5>
+                        <p>Experience the Kaleidoscope of Mexico: Savory Culinary Feasts, Sun-Kissed Beaches, Ancient Temples, and Rich History Unfold in Every Corner</p>
+                    </div>
+                    </a>
             </div>
         </div>
     </div>
-    
-
-
-
-
-
-
-
     <script src="/javascript/script1.js"></script>
 </body>
 </html>
