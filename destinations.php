388cb20bdd363cf37ce03a8718ed02b52685ab5d
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-18 14:58:15 +0100

    sub weggehaald css

--- destinations.php
+++ destinations.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Destinations</title>
     <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
-    <link href="css/stylesheetsubpages.css" rel="stylesheet">
+    <!-- <link href="css/stylesheetsubpages.css" rel="stylesheet"> -->
     <link href="css/stylesheet.css/" rel="stylesheet">
 </head>
 <body>
