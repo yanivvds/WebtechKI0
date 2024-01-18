4a1f21e6adda3164c9e3755b3b8350203e1d314b
”Thierry <thierry.schneider@student.uva.nl>
2024-01-18 15:13:47 +0100

    central america toegevoegd

--- destinations.php
+++ destinations.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Destinations</title>
     <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
-    <link href="css/stylesheetsubpages.css" rel="stylesheet">
+    <link href="css/destinations.css" rel="stylesheet">
 </head>
 <body>
     <?php include 'navbar.php'; ?>
+    
 </body>
 </html>
