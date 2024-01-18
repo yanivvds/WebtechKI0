afa1e7f8ca38a7ce439bff6371b37b34b82091d4
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-18 22:44:35 +0100

    sign up page succes!

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ session_start();
                 </div>
         </div>
     <script src="/javascript/script1.js"></script>
-    <?php /* require_once("footer.php");  */ ?>
+    <?php /* require_once("footer.php");  */?>
 
 </body>
 </html>
