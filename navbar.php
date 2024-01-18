ac682d4cb565349280feb2ffa36ffb19c5e62c75
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-18 17:19:43 +0100

    logo toegevoegd

--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@
 <body>
   <nav>
     <div class="wrapper">
-    <div class="logo"><a href="index.php">Logo</a></div>
+    <div class="logo"><a href="index.php"><img src="/fotos/logomain.ong" alt="Logo"></a></div>
       <input type="radio" name="slider" id="menu-btn">
       <input type="radio" name="slider" id="close-btn">
       <ul class="nav-links">
