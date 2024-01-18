f326c124b628670b1d7b6ad0323741169f20b397
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-18 15:05:43 +0100

    dest

--- destinations.php
+++ destinations.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Destinations</title>
     <link rel="stylesheet" href="/css/stylesheet.css"> 
+    <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
 </head>
 <body>
 <div>
