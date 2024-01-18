e99810765f67ff7006f45d06598d0e199b97ece1
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-18 20:18:41 +0100

    wereldkaart!!

--- destinations.php
+++ destinations.php
@@ -8,9 +8,36 @@
     <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
     
 </head>
-<body>
+<body style="background: white;>
 <div>
 <?php require_once("navbar.php"); ?>
 </div>
+<div class="world-map">
+    <img src="/wereld/wereld.png" alt="wereldkaart" />
+    <div class="pin guatemala">
+        <span>Guatemala</span>
+    </div>
+    <div class="pin costa-rica">
+        <span>Costa Rica</span>
+    </div>
+    <div class="pin australia">
+        <span>Australia</span>
+    </div>
+    <div class="pin italy">
+        <span>Italy</span>
+    </div>
+    <div class="pin portugal">
+        <span>Portugal</span>
+    </div>
+    <div class="pin norway">
+        <span>Norway</span>
+    </div>
+    <div class="pin usa">
+        <span>usa</span>
+    </div>
+    <div class="pin belize">
+        <span>Belize</span>
+    </div>
+
 </body>
 </html>
