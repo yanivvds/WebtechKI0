c416d0baa2587caf3c3e890db58708b7f2a2db84
”Thierry <thierry.schneider@student.uva.nl>
2024-01-20 00:44:54 +0100

    central america nieuw

---
+++ centralamerica.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Central America</title>
+    <link rel="icon" type="image/x-icon" href="/fotos/favicon.ico">
+    <link href="css/centralamerica.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="ImageBackgroundWrap Background-fixed" style="Background-image: url( ../fotos/panama.jpg)">
+        <div class="ImageInnerWrap">
+            <div class="TextBackground">
+                <div><h1>Central America</h1></div>
+            </div>
+        </div>
+    </div>
+    <div class="capage">
+        <div class="ca-container">
+            <div class="grid-pic1">
+                <img src="../fotos/beachportrait.jpg" class="beachleft">
+            </div>
+            <div class="grid-text1">
+                <h1>Travelling to<br> Central America</h1>
+                <p>Central America, a region connecting North and South America,<br> is a tapestry of diverse landscapes, vibrant cultures, and rich history.<br>
+                Comprising seven countries:<br> Belize, Costa Rica, El Salvador, Guatemala, Honduras, Nicaragua, and Panama.<br>Central America offers a kaleidoscope of experiences for travelers.<br>
+                From the ancient Mayan ruins in Guatemala<br> to the pristine beaches of Costa Rica and the lush rainforests of Belize,<br> each country is a unique gem waiting to be explored.<br>
+                Dive into the warmth of local traditions, savor delicious cuisines,<br>and embark on adventures that range from <br>exploring volcanoes to lounging on tropical shores.<br>
+                Central America beckons with its natural beauty, cultural richness,<br>and a warm welcome that leaves a lasting impression on every visitor.
+                </p>
+            </div>
+            <div class="grid-text2">
+                <h1>Top 5 spots<br>you need to visit!</h1>    
+                <ol>
+                    <li>
+                    <strong>Tikal, Guatemala:</strong> Explore the ancient Mayan city of Tikal, nestled in the jungles of Guatemala. Climb towering pyramids for panoramic views of the lush surroundings and immerse yourself in the rich history of this archaeological marvel.
+                    </li>
+                    <li>
+                    <strong>Arenal Volcano, Costa Rica:</strong> Witness the majesty of Arenal Volcano in Costa Rica. Surrounded by lush rainforests, the volcano offers hiking trails, hot springs, and the opportunity to relax in thermal baths with a stunning view of the active peak.
+                    </li>
+                    <li>
+                    <strong>Caye Caulker, Belize:</strong> Experience the laid-back charm of Caye Caulker in Belize. This idyllic island paradise is known for its crystal-clear waters, vibrant coral reefs, and a relaxed atmosphere. Snorkel or dive in the Great Blue Hole, a UNESCO World Heritage Site.
+                    </li>
+                    <li>
+                    <strong>Antigua, Guatemala:</strong> Step into the colonial beauty of Antigua, Guatemala. This UNESCO World Heritage Site boasts well-preserved Spanish Baroque-influenced architecture, cobblestone streets, and vibrant markets. It's a cultural hub surrounded by volcanoes.
+                    </li>
+                    <li>
+                    <strong>Manuel Antonio National Park, Costa Rica:</strong> Discover the biodiversity of Manuel Antonio National Park in Costa Rica. This coastal gem combines tropical rainforests with pristine beaches. Encounter diverse wildlife, including monkeys and sloths, as you hike the park's trails.
+                    </li>
+                </ol>
+            </div>
+            <div class="grid-pic2">
+                <div class="picture-container">
+                    <div class="picture-pic1"></div>
+                    <div class="picture-pic2"></div>
+                    <div class="picture-pic3"> </div>
+                    <div class="picture-pic4"></div>
+                    <div class="picture-pic5"></div>
+                  </div>
+            </div>
+            <div class="card">
+                <img class="tempel" src="../fotos/guatamala.jpg"  style="width:100%">
+                <div class="container">
+                  <h4><b>Jane Doe</b></h4> 
+                  <p>Interior Designerdsnldks</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="tempel" src="../fotos/tikal.jpg"  style="width:100%">
+                <div class="container">
+                  <h4><b>Jane Doe</b></h4> 
+                  <p>Interior Designer</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="tempel" src="../fotos/tempel.jpg"  style="width:100%">
+                <div class="container">
+                  <h4><b>Jane Doe</b></h4> 
+                  <p>Interior Designer</p> 
+                </div>
+            </div>
+            <div class="card">
+                <img class="tempel" src="../fotos/tempel.jpg"  style="width:100%">
+                <div class="container">
+                  <h4><b>Jane Doe</b></h4> 
+                  <p>Interior Designer</p> 
+                </div>
+            </div>
+          </div>
+    </div>   
+</body>
+</html>
+
