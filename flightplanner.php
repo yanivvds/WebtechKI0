b1576afe0ab074e5d69004ad582ae72a9851be21
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-23 13:55:13 +0100

    flight planner

--- flightplanner.php
+++ flightplanner.php
@@ -20,6 +20,26 @@ if (session_status() == PHP_SESSION_NONE) {
 <link rel="stylesheet" href="/css/stylesheet.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<style>
+        .ui-autocomplete {
+            background-color: #f0f0f0; /* Background color of the dropdown */
+            border: 1px solid #ccc;
+            max-height: 200px;
+            overflow-y: auto;
+            overflow-x: hidden;
+        }
+
+        .ui-menu-item {
+            padding: 3px 15px;
+            background-color: #f0f0f0; 
+            color: #333; /* Text color */
+        }
+
+        .ui-menu-item:hover, .ui-menu-item.ui-state-focus {
+            background-color: #d0d0d0; 
+            color: #212121; /* Hover text color */
+        }
+    </style>
 </head>
 <body style="height: 100vh;">
 <?php require_once("navbar.php"); ?>
