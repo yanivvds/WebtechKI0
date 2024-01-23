b2740223be80e413906f59778e6727c708290bad
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-23 10:34:32 +0100

    save

--- flights/save_flight.php
+++ flights/save_flight.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 include '/var/www/database.php';
 
@@ -7,18 +6,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $userID = $_SESSION["user_id"];
 }
 
+$airline = $_POST['airline'];
+$flightNumber = $_POST['flightNumber'];
+$departureAirport = $_POST['departureAirport'];
+$arrivalAirport = $_POST['arrivalAirport'];
+$departureDateTime = $_POST['departureDateTime'];
+$arrivalDateTime = $_POST['arrivalDateTime'];
+$ticketPrice = $_POST['ticketPrice'];
 
-
-$id = $_SESSION["user_id"];
-
-
-
-$sql = "INSERT INTO SavedFlights (UserID, FlightID) VALUES (?, ?)";
+$sql = "INSERT INTO Flight (Airline, FlightNumber, DepartureAirport, ArrivalAirport, DepartureDateTime, ArrivalDateTime, TicketPrice) VALUES (?, ?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ii", $id, $flightID);
+$stmt->bind_param("ssssssd", $airline, $flightNumber, $departureAirport, $arrivalAirport, $departureDateTime, $arrivalDateTime, $ticketPrice);
 
 if ($stmt->execute()) {
-   $response = ['success' => true];
+   $flightID = $stmt->insert_id;
+
+   $sql = "INSERT INTO UserFlights (UserID, FlightID) VALUES (?, ?)";
+   $stmt = $conn->prepare($sql);
+   $stmt->bind_param("ii", $userID, $flightID);
+
+   if ($stmt->execute()) {
+       $response = ['success' => true];
+   } else {
+       $response = ['success' => false];
+   }
 } else {
    $response = ['success' => false];
 }
