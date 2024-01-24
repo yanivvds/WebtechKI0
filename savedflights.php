702c03e447942092ac914972d117d091abc5036f
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-24 13:44:58 +0100

    euro teken

--- savedflights.php
+++ savedflights.php
@@ -71,7 +71,7 @@
                 echo "<td>" . htmlspecialchars($row['ArrivalAirport']) . "</td>";
                 echo "<td>" . htmlspecialchars($row['DepartureDateTime']) . "</td>";
                 echo "<td>" . htmlspecialchars($row['ArrivalDateTime']) . "</td>";
-                echo "<td>" . htmlspecialchars($row['Ticketprice']) . "</td>";
+                echo "<td>€" . htmlspecialchars($row['Ticketprice']) . "</td>";
                 echo "</tr>";
             }
             echo "</table>";
