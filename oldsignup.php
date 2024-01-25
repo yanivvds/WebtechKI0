602b16983f941b15ad3e33b52ca18cc40f276561
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-25 10:17:44 +0100

    signup fix

--- oldsignup.php
+++ oldsignup.php
@@ -1,75 +1,14 @@
-<?php
-$errorMessage = '';
-$is_invalid = false;
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (empty($_POST["username"])) {
-        $errorMessage = "Name is required";
-        $is_invalid = true;
-    }
-
-    if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-        $errorMessage = "Valid email is required";
-        $is_invalid = true;
-    }
-
-    if (strlen($_POST["password"]) < 8) {
-        $errorMessage = "Password must be at least 8 characters";
-        $is_invalid = true;
-    }
-
-    if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
-        $errorMessage = "Password must contain at least one letter";
-            $is_invalid = true;
-    }
-
-    if ( ! preg_match("/[0-9]/", $_POST["password"])) {
-        $errorMessage = "Password must contain at least one number";
-            $is_invalid = true;
-    }
-
-    if ($_POST["password"] !== $_POST["password_confirmation"]) {
-        $errorMessage = "Passwords have to be the same";
-        $is_invalid = true;
-    } else {
-    $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
-    $mysqli = require __DIR__ . "/../database.php";
-
-    $sql = "INSERT INTO Users (username, email, password_hash) VALUES (?, ?, ?)";
-
-    $stmt = $mysqli->stmt_init();
-    }
-}
-
-
-$stmt->bind_param("sss", $_POST["username"], $_POST["email"], $password_hash);
-
-if ($stmt->execute()) {
-
-    header("Location: signupsucces.html");
-    exit;
-    
-} else {
-
-    if ($stmt->errno === 1062) {
-        $errorMessage = "Email has already been used.";
-        $is_invalid = true;
-    } else {
-        die($mysqli->error . " " . $mysqli->errno);
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign up</title>
+    <title>Sign-up</title>
+    <link rel="stylesheet" href="/css/stylesheet.css">
 </head>
 <body>
 <?php require_once("navbar.php"); ?>
+</div>
     <div class="form-container" style="display: flex; justify-content: center; align-items: center; height: 100vh;">
         <form action="signupscript.php" method="post" class="formsignup">
             <div class="title">Sign-up</div>
@@ -101,5 +40,7 @@ if ($stmt->execute()) {
             <button type="submit" class="submit">Sign-up</button>
         </form>
     </div>
+    
+    
 </body>
 </html>
