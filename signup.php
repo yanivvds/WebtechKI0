1a29f9f6de92a6dddcefb4cedc5ca66e1a60751c
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-25 10:25:47 +0100

    stylesheet

--- signup.php
+++ signup.php
@@ -67,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign up</title>
+    <href rel="stylesheet" href="/css/stylesheet.css">
 </head>
 <body>
 <?php require_once("navbar.php"); ?>
