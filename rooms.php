72ff5945bb3376c6564c771f5013b7dae975e210
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-26 22:51:26 +0100

    background color

--- rooms.php
+++ rooms.php
@@ -33,6 +33,7 @@
 
     .hotel-offer {
         border: 1px solid #ddd;
+        background-color: #f0f8ff33;
         padding: 20px;
         border-radius: 5px;
         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
