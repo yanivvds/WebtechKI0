17336f4a5a96a0607d2ac534f736216c3b013d6f
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-26 20:47:40 +0100

    error log

--- save_room.php
+++ save_room.php
@@ -12,7 +12,7 @@ header('Content-Type: application/json');
 include('hotelapi.php');
 include('../database.php');
 
-var_dump($_POST);
+error_log(print_r($_POST, true));
 
 if (!isset($_SESSION["user_id"])) {
 } else {
