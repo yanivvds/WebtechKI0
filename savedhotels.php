4eeff550cc9cbd5dadc5e7ace4f72cd2dc17aa45
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-26 22:16:15 +0100

    border radius

--- savedhotels.php
+++ savedhotels.php
@@ -32,7 +32,7 @@
             font-size: 18px;
             font-weight: bold;
             text-align: center;
-            border-radius: 15px 15px 0 0;
+            border-radius: 15px 15px 15px 15px;
         }
 
         .card-body {
