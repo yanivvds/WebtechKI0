466a2fdf4db230db32bc3c66ee6b8250b8cd9895
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-27 21:34:57 +0100

    price

--- experience_search.php
+++ experience_search.php
@@ -146,7 +146,8 @@ $longitude = $_POST['longitude'] ?? '2.173691'; // Default longitude
 $radius = 4; // 4 km radius
 $minPrice = $_POST['minPrice'] ?? 0;
 $maxPrice = $_POST['maxPrice'] ?? 5000;
-
+console_log($minPrice);
+console_log($maxPrice);
 
 $ch = curl_init();
 
