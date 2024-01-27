16cc69bfd744f90f0a99cee2d1dd53af992a2bae
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-27 22:01:23 +0100

    vardump

--- experience_search.php
+++ experience_search.php
@@ -159,6 +159,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
+var_dump($response);
 $responseArray = json_decode($response, true);
 
 curl_close($ch);
