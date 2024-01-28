3bb6a310d09775ea885504f594efd891126ee2d3
”Thierry <thierry.schneider@student.uva.nl>
2024-01-29 00:01:39 +0100

    asia tekst gefixt

--- asia.php
+++ asia.php
@@ -26,10 +26,10 @@
             </div>
             <div class="grid-text1">
                 <h1>Travelling to<br> Asia</h1>
-                <p>Asia, the largest and most diverse continent on Earth, is a mosaic of enchanting landscapes, captivating cultures, and millennia-old histories.<br>
-                Comprising a vast expanse of nations, including but not limited to:<br> China, India, Japan, Thailand, Vietnam, Indonesia, and many more,<br> Asia promises a captivating journey for every type of traveler.<br>
-                From the iconic Great Wall of China to the serene beaches of Thailand and the bustling streets of Tokyo,<br> each country in Asia unfolds a unique narrative waiting to be discovered.<br>
-                Immerse yourself in the splendor of ancient temples, savor the aromas of diverse cuisines,<br>and engage in adventures that range from trekking through the Himalayas to relaxing on tropical islands.<br>
+                <p>Asia, the largest and most diverse continent on Earth, is a mosaic of enchanting landscapes, captivating cultures and centuries worth of history.<br>
+                    It consists of a vast expanse of countries including but not limited to:<br> China, India, Japan, Thailand, Vietnam, Indonesia, and many more,<br> Asia promises a captivating journey for every type of traveler.<br>
+                From the iconic Great Wall of China to the serene beaches of Thailand and the bustling streets of Tokyo<br> Each country in Asia unfolds a unique narrative waiting to be discovered.<br>
+                Immerse yourself in the beauty of ancient temples, enjoy the aromas of diverse cuisines<br>and engage in adventures that range from trekking through the Himalayas to relaxing on tropical islands.<br>
                 Asia invites exploration with its breathtaking landscapes, cultural diversity,<br> and the genuine hospitality that creates unforgettable memories for every visitor.
                 </p>
             </div>
@@ -37,19 +37,19 @@
                 <h1>Top 5 Destinations<br>you need to explore in Asia!</h1>
                 <ol>
                     <li>
-                        <strong>Great Wall of China, China:</strong> Embark on a journey to the iconic Great Wall of China, winding its way through picturesque landscapes. Climb the ancient watchtowers for breathtaking views and immerse yourself in the rich history spanning centuries.
+                        <strong>Great Wall of China, China:</strong> Take a journey to the iconic Great Wall of China, which weaves its way through picturesque landscapes. Climb the ancient watchtowers for breathtaking views and immerse yourself in the rich ancient history.
                     </li>
                     <li>
-                        <strong>Taj Mahal, India:</strong> Witness the timeless beauty of the Taj Mahal in India. This architectural masterpiece, with its intricate marble design, stands as a symbol of eternal love. Explore the surrounding gardens and marvel at the grandeur of this UNESCO World Heritage Site.
+                        <strong>Taj Mahal, India:</strong> Witness the timeless beauty of the Taj Mahal in India. This architectural masterpiece, with its intricate marble design, symbolizes eternal love. Explore the surrounding gardens and admire the grandeur of this UNESCO World Heritage Site.
                     </li>
                     <li>
-                        <strong>Mount Fuji, Japan:</strong> Experience the awe-inspiring presence of Mount Fuji in Japan. This iconic volcano is a symbol of natural beauty and tranquility. Whether you admire it from a distance or embark on a hiking adventure, Mount Fuji promises a memorable experience.
+                        <strong>Mount Fuji, Japan:</strong> Experience the awe-inspiring presence of Mount Fuji in Japan. This iconic volcano is a symbol of natural beauty and tranquility. Whether you admire it from a distance or embark on a hiking adventure, Mount Fuji promises an unforgettable experience.
                     </li>
                     <li>
-                        <strong>Phi Phi Islands, Thailand:</strong> Escape to the paradise of Phi Phi Islands in Thailand. With crystal-clear waters, vibrant coral reefs, and stunning limestone cliffs, these islands offer a tropical haven. Snorkel in emerald waters or relax on pristine beaches surrounded by lush greenery.
+                        <strong>Phi Phi Islands, Thailand:</strong> Escape to the paradise of Thailand's Phi Phi Islands. With crystal clear waters, vibrant coral reefs and beautiful limestone cliffs, these islands offer a tropical retreat. Snorkel in emerald green waters or relax on pristine beaches surrounded by lush greenery.
                     </li>
                     <li>
-                        <strong>Bali, Indonesia:</strong> Immerse yourself in the enchanting beauty of Bali, Indonesia. Known for its lush landscapes, ancient temples, and vibrant culture, Bali offers a perfect blend of relaxation and adventure. Explore rice terraces, visit sacred temples, and unwind on stunning beaches.
+                        <strong>Bali, Indonesia:</strong> Immerse yourself in the enchanting beauty of Bali, Indonesia. Known for its lush landscapes, ancient temples and vibrant culture, Bali offers a perfect blend of relaxation and adventure. Explore rice terraces, visit sacred temples and relax on beautiful beaches.
                     </li>
                 </ol>
             </div>
@@ -66,32 +66,32 @@
                 <img src="../fotos/china.jpg" alt="china">
                 <div class="card-content">
                     <h2>China</h2>
-                    <p>China, a nation with a 5,000-year history, seamlessly blends ancient traditions with modern progress, showcasing a diverse tapestry of culture and landscapes.</p>
-                    <a href="#">Book now &rArr;</a>
+                    <p>A country with a history spanning 5,000 years, China seamlessly blends ancient traditions with modern progress, presenting a diverse range of culture and landscapes.</p>
+                    <a href="hotelbooker.php">Book now &rArr;</a>
                 </div>
             </div>
             <div class="card">  
                 <img src="../fotos/japan.jpg" alt="japan">
                 <div class="card-content">
                     <h2>Japan</h2>
-                    <p>Japan, a blend of tradition and technology, offers a mesmerizing mix of ancient beauty and modern vibrancy.</p>
-                    <a href="#">Book now &rArr;</a>
+                    <p>A blend of tradition and technology, Japan offers an enchanting blend of ancient beauty and modern vibrancy.</p>
+                    <a href="hotelbooker.php">Book now &rArr;</a>
                 </div>
             </div>
             <div class="card">
                 <img src="../fotos/vietnam.jpg" alt="vietnam">
                 <div class="card-content">
                     <h2>Vietnam</h2>
-                    <p>Vietnam, a land of scenic beauty and ancient charm, weaves together vibrant culture and warm hospitality, making it a captivating destination.</p>
-                    <a href="#">Book now &rArr;</a>
+                    <p>A land of scenic beauty and age-old charm, Vietnam combines vibrant culture and warm hospitality, making it a captivating destination.</p>
+                    <a href="hotelbooker.php">Book now &rArr;</a>
                 </div>
             </div>
             <div class="card">
                 <img src="../fotos/indonesia.jpg" alt="indonesia">
                 <div class="card-content">
                     <h2>Indonesia</h2>
-                    <p>Indonesia, a tropical paradise of diverse islands, seamlessly combines natural beauty with vibrant cultures, creating a captivating destination.</p>
-                    <a href="#">Book now &rArr;</a>
+                    <p>A tropical paradise with diverse islands, Indonesia seamlessly blends natural beauty with vibrant cultures, creating a captivating destination.</p>
+                    <a href="hotelbooker.php">Book now &rArr;</a>
                 </div>
             </div>
             </div>
