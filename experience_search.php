e9c0f39033b7663942f7571ca427d41372e2ad81
Yaniv van der Stigchel <yaniv.van.der.stigchel@student.uva.nl>
2024-01-30 15:56:38 +0100

    max requests

--- experience_search.php
+++ experience_search.php
@@ -138,7 +138,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-define('MAX_REQUESTS', 30); // Max requests per 60s
+define('MAX_REQUESTS', 3); // Max requests per 60s
 define('TIME_WINDOW', 60);
 
 // Initialise the session timer
